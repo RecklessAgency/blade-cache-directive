505b4a7ae13d786b807988994e1637b69af9f892
Bryn Jones <bryn.jones@reckless.agency>
2024-04-22 22:10:07 +0100

    Add support for cache tags

--- src/BladeCacheDirectiveServiceProvider.php
+++ src/BladeCacheDirectiveServiceProvider.php
@@ -3,6 +3,7 @@
 namespace RyanChandler\BladeCacheDirective;
 
 use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\Cache;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
@@ -20,32 +21,50 @@ class BladeCacheDirectiveServiceProvider extends PackageServiceProvider
         Blade::directive('cache', function ($expression) {
             return "<?php
                 \$__cache_directive_arguments = [{$expression}];
+                \$__cache_directive_tags = []; // Default empty tags
 
-                if (count(\$__cache_directive_arguments) === 2) {
+                if (count(\$__cache_directive_arguments) === 3) {
+                    [\$__cache_directive_key, \$__cache_directive_ttl, \$__cache_directive_tags] = \$__cache_directive_arguments;
+                } elseif (count(\$__cache_directive_arguments) === 2) {
                     [\$__cache_directive_key, \$__cache_directive_ttl] = \$__cache_directive_arguments;
                 } else {
                     [\$__cache_directive_key] = \$__cache_directive_arguments;
                     \$__cache_directive_ttl = config('blade-cache-directive.ttl');
                 }
 
-                if (config('blade-cache-directive.enabled') && \Illuminate\Support\Facades\Cache::has(\$__cache_directive_key)) {
-                    echo \Illuminate\Support\Facades\Cache::get(\$__cache_directive_key);
-                } else {
-                    \$__cache_directive_buffering = true;
+                if (config('blade-cache-directive.enabled')) {
+                    if (!empty(\$__cache_directive_tags)) {
+                        \$cacheStore = Cache::tags(\$__cache_directive_tags);
+                    } else {
+                        \$cacheStore = Cache::store();
+                    }
 
-                    ob_start();
+                    if (\$cacheStore->has(\$__cache_directive_key)) {
+                        echo \$cacheStore->get(\$__cache_directive_key);
+                    } else {
+                        \$__cache_directive_buffering = true;
+                        ob_start();
+                    }
+                }
             ?>";
         });
 
         Blade::directive('endcache', function () {
             return "<?php
-                    \$__cache_directive_buffer = ob_get_clean();
+                    if (isset(\$__cache_directive_buffering) && \$__cache_directive_buffering) {
+                        \$__cache_directive_buffer = ob_get_clean();
 
-                    \Illuminate\Support\Facades\Cache::put(\$__cache_directive_key, \$__cache_directive_buffer, \$__cache_directive_ttl);
+                        if (!empty(\$__cache_directive_tags)) {
+                            \$cacheStore = Cache::tags(\$__cache_directive_tags);
+                        } else {
+                            \$cacheStore = Cache::store();
+                        }
 
-                    echo \$__cache_directive_buffer;
+                        \$cacheStore->put(\$__cache_directive_key, \$__cache_directive_buffer, \$__cache_directive_ttl);
+                        echo \$__cache_directive_buffer;
+                    }
 
-                    unset(\$__cache_directive_key, \$__cache_directive_ttl, \$__cache_directive_buffer, \$__cache_directive_buffering, \$__cache_directive_arguments);
+                    unset(\$__cache_directive_key, \$__cache_directive_ttl, \$__cache_directive_buffer, \$__cache_directive_buffering, \$__cache_directive_arguments, \$__cache_directive_tags, \$cacheStore);
                 }
             ?>";
         });
